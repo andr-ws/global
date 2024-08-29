79a7d136c8a938a4009c552ad039c67f10494bba
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-08-29 12:26:59 +0100

    Create anat_processing.sh

---
+++ anat_processing.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+
+# Raw T1w/T2w handling, minimal pre-processing (reorientation, neck crop and brain extraction)
+# Co-registration of T1w/T2w images
+
+# Prompt the base path
+read -p "Enter the base path of the dataset (e.g., ./path/2/data): " base
+
+# Contains cross-purposes files (i.e., MNI spaces)
+global=./imaging/global
+
+# Base directory structure
+rawdata="${base}/raw_data"
+derivatives="${base}/derivatives"
+
+find "${rawdata}" -type d -name 'sub-*' | sort -V | while read -r dir; do
+  # extract subject-id and create directory
+  sub=$(basename "${dir}")
+
+  mkdir -p "${derivatives}/data/${sub}/anat"
+
+  for modality in T1w T2w; do
+
+    echo "Reorienting and cropping ${sub}..."
+    fslreorient2std \
+    "${rawdata}/${sub}/anat/${sub}_${modality}.nii.gz" \
+    "${derivatives}/data/${sub}/anat/${sub}_desc-min_proc_${modality}.nii.gz"
+	
+    robustfov \
+    -i "${derivatives}/data/${sub}/anat/${sub}_desc-min_proc_${modality}.nii.gz" \
+    -r "${derivatives}/data/${sub}/anat/${sub}_desc-min_proc_${modality}.nii.gz"
+
+    echo "Biasfield correcting ${sub}..."
+    N4BiasFieldCorrection \
+    -d 3 \
+    -i "${derivatives}/data/${sub}/anat/${sub}_desc-min_proc_${modality}.nii.gz" \
+    -o "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_${modality}.nii.gz"
+
+    # brain extraction
+    echo "Brain extracting (minimal and biasfield) for ${sub}..."
+    for img in min_proc bias_cor; do
+      mri_synthstrip \
+      --image "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}.nii.gz" \
+      --out "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}_brain.nii.gz" \
+      --mask "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}_brain_mask.nii.gz"
+    done # end brain extraction
+  done # end modality loop
+
+  # Co-register T2w-T1w
+
+  
+
+  
+done # end participant loop
