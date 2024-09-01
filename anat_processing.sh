d7eb121fa13e25c0ff549ab29f577bf4a57d4d8e
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-09-01 17:33:03 +0100

    Update anat_processing.sh
    Added the ability to resample (--resample) and the necessary processing if selected.

--- anat_processing.sh
+++ anat_processing.sh
@@ -13,14 +13,23 @@ global=./imaging/global
 rawdata="${base}/rawdata"
 derivatives="${base}/derivatives"
 
+# Add a flag here: --resample to resample both T1 and T2w MRI to 1mm isotropic
+
+# Parse optional resampling arg
+resample=false
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --resample) resample=true ;;
+    esac
+    shift
+done
+
 find "${rawdata}" -type d -name 'sub-*' | sort -V | while read -r dir; do
   # extract subject-id and create directory
   sub=$(basename "${dir}")
-
   mkdir -p "${derivatives}/data/${sub}/anat"
 
   for modality in T1w T2w; do
-
     echo "Reorienting and cropping ${sub}..."
     fslreorient2std \
     "${rawdata}/${sub}/anat/${sub}_${modality}.nii.gz" \
@@ -36,25 +45,53 @@ find "${rawdata}" -type d -name 'sub-*' | sort -V | while read -r dir; do
     -i "${derivatives}/data/${sub}/anat/${sub}_desc-min_proc_${modality}.nii.gz" \
     -o "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_${modality}.nii.gz"
 
-    # brain extraction
-    echo "Brain extracting (minimal and biasfield) for ${sub}..."
-    for img in min_proc bias_cor; do
-      mri_synthstrip \
-      --image "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}.nii.gz" \
-      --out "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}_brain.nii.gz" \
-      --mask "${derivatives}/data/${sub}/anat/${sub}_desc-${img}_${modality}_brain_mask.nii.gz"
-    done # end brain extraction
+    # Resample if specified
+    if [ "$resample" = true ]; then
+    	echo "Resampling ${sub} to 1mm isotropic..."
+	flirt \
+	-in "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_${modality}.nii.gz" \
+    	-ref "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_${modality}.nii.gz" 
+    	-applyisoxfm 1.0 \
+    	-nosearch \
+    	-out "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_iso_1mm_${modality}.nii.gz"
+
+     	# Use the resampled image for further processing
+      	modality_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_iso_1mm_${modality}.nii.gz"
+     else
+       	# Use the non-resampled image for further processing
+	modality_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_${modality}.nii.gz"
+     fi 
+    
+    echo "Brain extracting ${sub}..."
+    mri_synthstrip \
+    --image "${modality_image}" \
+    --out "${modality_image%.*}_brain.nii.gz" \
+    --mask "${modality_image%.*}_brain_mask.nii.gz"
+
   done # end modality loop
 
-  # Co-register T2w-T1w MRI
+  # Determine the correct filenames for co-registration
+  if [ "$resample" = true ]; then
+    t1_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_iso_1mm_T1w_brain.nii.gz"
+    t2_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_iso_1mm_T2w_brain.nii.gz"
+    coreg_out="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_space-T1w_iso_1mm"
+  else
+    t1_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T1w_brain.nii.gz"
+    t2_image="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_brain.nii.gz"
+    coreg_out="${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_space-T1w"
+  fi
+
+  # Co-register T2w to T1w MRI
+  echo "Co-registering T2w to T1w for ${sub}..."
   antsRegistrationSyNQuick.sh \
   -d 3 \
-  -f "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T1w_brain.nii.gz" \
-  -m "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_brain.nii.gz" \
-  -o "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_space-T1w_"
+  -f "${t1_image}" \
+  -m "${t2_image}" \
+  -o "${coreg_out}"
 
-  # Rename co-registered T2
-  mv "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_space-T1w_Warped.nii.gz" \
-  "${derivatives}/data/${sub}/anat/${sub}_desc-bias_cor_T2w_space-T1w.nii.gz"
+  # Rename co-registered T2w
+  mv "${coreg_out}Warped.nii.gz" "${coreg_out}.nii.gz"
   
 done # end participant loop
+
+
