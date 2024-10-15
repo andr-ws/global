609cb6b2bf41d01e2f51646112e0909b7dcf0519
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-10-15 13:37:32 +0100

    Update and rename raghoo to raghoo.sh

--- raghoo.sh
+++ raghoo.sh
@@ -1,6 +1,11 @@
-# ADD SLURM COMMANDS HERE
+#!/bin/bash
 
-
+#SBATCH --job-name=recon-all_raghoo2k
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=8  # Adjust according to your needs
+#SBATCH --mem=24GB         # Adjust according to your needs
+#SBATCH --time=48:00:00    # Adjust according to your needs
 
 # Base directory structure
 basedir="/users/raghoo2k/volatile/Der"
